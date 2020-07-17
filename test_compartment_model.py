8b48f2847858b87a01adc9a97a53c296c0cc7961
Claire Valva <clairev@uchicago.edu>
2020-07-17 14:11:21 -0700

    code update, works but not clean

--- test_compartment_model.py
+++ test_compartment_model.py
@@ -1,24 +1,17 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[244]:
 
 
 # imports
 import numpy as np
 import scipy as scp
 import scipy.integrate as sint
+import matplotlib.pyplot as plt
 
 
 # In[2]:
-
-
-# define beta
-beta = np.ones((3,3))
-gamma = 1
-
-
-# In[3]:
 
 
 def generate_S(n = 3, scale = 1):
@@ -27,7 +20,7 @@ def generate_S(n = 3, scale = 1):
     return arr*ex
 
 
-# In[4]:
+# In[3]:
 
 
 def generate_I(n = 3, initial = 1):
@@ -35,7 +28,7 @@ def generate_I(n = 3, initial = 1):
     return arr
 
 
-# In[5]:
+# In[4]:
 
 
 def generate_R(n = 3):
@@ -43,7 +36,7 @@ def generate_R(n = 3):
     return arr.astype(float)
 
 
-# In[6]:
+# In[217]:
 
 
 def initial_counts(n = 3, scale = 1, initial = 1):
@@ -56,37 +49,124 @@ def initial_counts(n = 3, scale = 1, initial = 1):
     return np.array([S,I,R])
 
 
-# In[7]:
+# In[218]:
 
 
-test = initial_counts(n = 3, scale = 1, initial = 1)
-
-
-# In[8]:
-
-
-def f(t, y):
-    B = np.array([[1,1,1], [1,1,1], [1,1,1]]).astype(float)
-    gamma = 1
-    n = 3
-    S = np.array([y[:n].astype(float)])
-    I = np.transpose([y[n:2*n].astype(float)])
-    R = y[2*n:].astype(float)
+def compute_sdot(S,I,R,beta,C):
+    '''
+    S,I ,R, are n by 1 vectors, beta is a constant,
+    C is an n by n matrix
+    '''
     
-    oneS = np.matmul(np.ones((n,1)), S)
+    inv_pop = np.divide(np.ones_like(S + I + R, dtype = float), S + I + R)
     
-    sdot = -1*np.matmul(B,np.matmul(oneS,I)) #3x1 in this case
-    idot = sdot - gamma*I #3x1 in this case
-    rdot = gamma*I #3x1 in this case
+    sdot = [-beta*S[i]*np.sum(np.multiply(C[i],
+                                          np.multiply(I,inv_pop)))
+            for i in range(len(S))]
+    return np.array(sdot)    
+
+
+# In[219]:
+
+
+def compute_idot(S,I,R,beta,C,gamma):
+    sdot = -1*compute_sdot(S,I,R,beta,C) 
+    return sdot - gamma*I
+
+
+# In[220]:
+
+
+def compute_rdot(I,gamma):
+    return gamma*I
+
+
+# In[367]:
+
+
+beta = 0.5
+n = 2
+C = [[10.,.8],[.8,1.]]
+
+
+# In[353]:
+
+
+S = [33500,168000-33499]
+I = [0,1]
+R = [0,0]
+y_0 = np.array([S,I,R]).flatten()
+
+
+# In[354]:
+
+
+def f(t,y):
+    S = y[:n]
+    I = y[n:2*n]
+    R = y[2*n:3*n]
     
-    bad = [entry.flatten() for entry in [sdot, idot, rdot]]
-  
-    return np.array(bad).flatten()
+    sdot = compute_sdot(S,I,R,beta,C)
+    idot = compute_idot(S,I,R,beta,C,gamma)
+    rdot = compute_rdot(I,gamma)
+    
+    return np.array([sdot,idot,rdot]).flatten()
 
 
-# In[9]:
+# In[368]:
 
 
-y_0 = initial_counts()
-y = sint.solve_ivp(f, [0,10], np.array(range(9)))
+pleasework = sint.solve_ivp(f,[0,20],y_0, max_step = 0.01)
+
+
+# In[369]:
+
+
+shaped = np.reshape(pleasework.y, (3,n,len(pleasework.t)))
+
+
+# In[370]:
+
+
+#comp1 = shaped[:,0,:]
+
+
+# In[374]:
+
+
+plt.clf()
+plt.scatter(pleasework.t,np.add(shaped[1,0,:], shaped[1,1,:]),label = "I, city")
+plt.scatter(pleasework.t,shaped[1,0,:],label = "I, university")
+plt.legend()
+plt.show()
+
+
+# In[362]:
+
+
+#sums = np.sum(comp1,axis = 0)
+
+
+# In[363]:
+
+
+compartment = 1
+plt.clf()
+plt.scatter(pleasework.t,shaped[0,compartment,:],label = "S")
+plt.scatter(pleasework.t,shaped[1,compartment,:],label = "I")
+plt.scatter(pleasework.t,shaped[2,compartment,:],label = "R")
+plt.legend()
+plt.show()
+
+
+# In[364]:
+
+
+compartment = 0
+plt.clf()
+plt.scatter(pleasework.t,shaped[0,compartment,:],label = "S")
+plt.scatter(pleasework.t,shaped[1,compartment,:],label = "I")
+plt.scatter(pleasework.t,shaped[2,compartment,:],label = "R")
+plt.legend()
+plt.show()
 
