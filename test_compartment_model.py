15adf167622ec0e0a61c45edc261cfd3c7ad5371
Colin Roberts <robertsp@rams.colostate.edu>
2020-07-23 10:17:51 -0600

    updated sir compartmental model

--- test_compartment_model.py
+++ test_compartment_model.py
@@ -8,18 +8,27 @@ import seaborn as sns
 import csv
 import pandas as pd
 from datetime import datetime, date
+import requests
 
 '''
 TODO: Allow for time varying C in the SIR model
 '''
 
+# Data
+start_date = '03-09-2020'
+end_date = '07-22-2020'
+date_format = "%m-%d-%Y"
+begin = datetime.strptime(start_date, date_format)
+finish = datetime.strptime(end_date, date_format)
+delta_days = finish - begin
+total_days = delta_days.total_seconds()/86400 #convert from seconds to days.
+
 # Parameters
 n = 2
-gamma = 0.5
-beta = 4
-C = [[.5, 0],[0,.5]]
-#C = [[10,.5],[.5,1]]
-t_range = [0, 50]
+gamma = 0.125
+beta = .15
+C = [[1, 0],[0,1]]
+t_range = [0, total_days] # Choose unit time for ease, change later with dates
 t_intervention = 6
 
 # Initial conditions
@@ -31,7 +40,7 @@ y_0 = np.array([S,I,R]).flatten()
 
 # Main
 # Run without intervention
-pleasework = sint.solve_ivp(methods.f, t_range, y_0, max_step = 0.01, args=(n, beta, gamma, C))
+pleasework = sint.solve_ivp(methods.f, t_range, y_0, max_step = 0.1, args=(n, beta, gamma, C))
 
 # Run with intervention
 #pleasework = sint.solve_ivp(methods.f_intervention, t_range, y_0, max_step = 0.01, args=(n, beta, gamma, C,t_intervention))
@@ -46,50 +55,54 @@ shaped = np.reshape(pleasework.y, (3,n,len(pleasework.t)))
 '''Fetch Fort Collins Data'''
 #######################################################################################################################
 
-# Make initial cases in Fort Collins = 0
+# Fetch from URL
+# url_cases = 'https://apps.larimer.org/api/covid/?t=1595446631841&gid=1219297132&csv=cases'
+# url_deaths = 'https://larimer-county-data-lake.s3-us-west-2.amazonaws.com/Public/covid/covid_deaths.csv'
+# r = requests.get(url, allow_redirects=True)
 
-# build = {'Date': [0], 'Cases': [0]}
-# fc_cases = pd.DataFrame(data=build)
+# Create a list of dates through today
+#date_list = pd.date_range(start = '03-09-2020', end = datetime.today())
+date_list = pd.date_range(start = start_date, end = end_date) # Chosen for a specific date
 
-#print(fc_cases)
-
+# Import the Larimer County confirmed/probable Cases
 larimer_cases = pd.read_csv('LC-COVID-casesdata.csv', sep=',', parse_dates=['ReportedDate'])
 
-date_list = pd.date_range(start = '03-09-2020', end = datetime.today())
-
-new_fc_cases = pd.DataFrame({'Cases':[0]*len(date_list)},index = date_list)
-
-#print(fc_cases)
-
+# Pull out the Fort Collins cases
+fc_cases = pd.DataFrame({'New Cases':[0]*len(date_list)},index = date_list)
 for i in larimer_cases.index:
     date = larimer_cases.at[i,'ReportedDate']
     if larimer_cases.at[i, 'City'] == 'Fort Collins':
-        new_fc_cases.at[date, 'Cases'] = new_fc_cases.at[date, 'Cases']+1
+        fc_cases.at[date, 'New Cases'] = fc_cases.at[date, 'New Cases']+1
 
-print(new_fc_cases)
+fc_cases['Total Cases'] = fc_cases['New Cases'].cumsum()
 
+# Import the Larimer County deaths
+larimer_deaths = pd.read_csv('LC-COVID-deathsdata.csv', sep=',', parse_dates=['death_date'])
+#print(larimer_deaths)
+#Pull out the Fort Collins confirmed/probable deaths
+fc_deaths = pd.DataFrame({'New Deaths':[0]*len(date_list)},index = date_list)
+for i in larimer_deaths.index:
+    date = larimer_deaths.at[i,'death_date']
+    if larimer_deaths.at[i, 'city'] == 'Fort Collins':
+        fc_deaths.at[date, 'New Deaths'] = fc_deaths.at[date, 'New Deaths']+1
 
-# with open('LC-COVID-casesdata.csv', newline='') as csvfile:
-#     reader = csv.reader(csvfile, delimiter=',', quotechar='|')
-#     for row in reader:
-#         df = pd.DataFrame({'Date': [row[1]], 'Cases': [0]})
-#         print(df)
-#         fc_cases.append(df)
-#         #print(row[1])
-#         #print(', '.join(row))
-#         if "Fort Collins" in row:
-#             x = 1
-#             #fc_cases = fc_cases + 1
-# print(fc_cases)
+fc_deaths['Total Deaths'] = fc_deaths['New Deaths'].cumsum()
 
+fc_data = pd.concat([fc_cases, fc_deaths], axis=1, sort=False)
+
+# Plot Fort Collins cases
+# fc_data.plot()
+# plt.show()
+
+#print(np.shape(shaped))
 #######################################################################################################################
 
 # Plot Model City vs. Real Data (Fort Collins, CO)
-# plt.clf()
-# sns.set_context("poster")
-# plt.scatter(pleasework.t,np.add(shaped[1,0,:], shaped[1,1,:]),label = "I, City")
-# plt.legend()
-# plt.show()
+plt.clf()
+sns.set_context("poster")
+plt.scatter(pleasework.t,np.add(shaped[1,0,:], shaped[1,1,:]),label = "I, City")
+plt.legend()
+plt.show()
 
 # Plot City and University
 # plt.clf()
