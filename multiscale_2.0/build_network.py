7608bccc166d058ca5fb83dff464b7d0c6bc2163
Colin Roberts <robertsp@rams.colostate.edu>
2020-10-08 15:54:19 -0600

    started new version 2.0

--- multiscale_2.0/build_network.py
+++ multiscale_2.0/build_network.py
@@ -2,3 +2,22 @@
 Takes in a population and returns a network (i.e., the adjacancy and degree matrices)
 '''
 
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def build_network(population):
+    total_nodes = population
+    nearest_neighbors = 2
+    prob_rewiring = 0.5
+
+    #G = nx.watts_strogatz_graph(n = total_nodes,
+    #                            k = nearest_neighbors,
+    #                            p = prob_rewiring)
+
+    G = nx.watts_strogatz_graph(n = total_nodes, k = nearest_neighbors, p = prob_rewiring, seed=4)
+
+    # Visualizing
+#    nx.draw(G, with_labels=True, font_weight='bold')
+#    plt.show()
+    return G
--- multiscale_2.0/solver.py
+++ multiscale_2.0/solver.py
@@ -4,3 +4,9 @@ Solves the diffusion problem on the time varying network.
 For a fixed interval draw a new network (e.g., each day).  Then, allow diffusion to occur at a small
  machine timestep.
 '''
+
+def dIdt(I, L, threshold):
+    threshold = .25
+    Idot = [L*I + 1 for i in range(len(I)) if I[i] > threshold]
+    return Idot
+
